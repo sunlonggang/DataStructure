8018ccc66f42adbc3b5159c3b865eb2d826a32ec
sunlonggang <longgang.sun@hjwang.com>
2017-04-20 09:30:42 +0800

    file

---
+++ ChainLinearList/ChainLinearList/main.c
@@ -0,0 +1,27 @@
+//
+//  main.c
+//  ChainLinearList
+//
+//  Created by slg on 2017/4/19.
+//  Copyright © 2017年 slg. All rights reserved.
+//
+
+#include <stdio.h>
+
+#define ElemType int
+
+typedef struct {
+    ElemType data;
+    struct Node* next;
+} Node;
+
+typedef struct Node* list;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+//    printf("Hello, World!\n");
+    
+    
+    
+    return 0;
+}
